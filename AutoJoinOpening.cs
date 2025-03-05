f7959e445f30eeca89aa332d97d590c1b2f700de
Maxim Dementev <dementevmax@gmail.com>
2025-03-05 11:32:58 +0300

    Добавьте файлы проекта.

---
+++ AutoJoinOpening.cs
@@ -0,0 +1,40 @@
+﻿using Autodesk.Revit.UI;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Events;
+using System.Linq;
+
+public class AutoJoinOpening : IExternalApplication
+{
+    private JoinCommand_EventHandler _joinCommand;
+
+    public Result OnStartup(UIControlledApplication application)
+    {
+        _joinCommand = new JoinCommand_EventHandler();
+
+        application.ControlledApplication.DocumentChanged += OnDocumentChanged;
+        return Result.Succeeded;
+    }
+
+    public Result OnShutdown(UIControlledApplication application)
+    {
+        application.ControlledApplication.DocumentChanged -= OnDocumentChanged;
+        return Result.Succeeded;
+    }
+
+    private void OnDocumentChanged(object sender, DocumentChangedEventArgs e)
+    {
+        Document doc = e.GetDocument();
+        if (doc == null) return;
+
+        var changedOrAddedElements = e.GetAddedElementIds()
+        .Concat(e.GetModifiedElementIds())
+        .Select(id => doc.GetElement(id))
+        .OfType<FamilyInstance>()
+        .Where(fi => fi.Symbol.Family.Name.StartsWith("Отверстие_Плита_"))
+        .ToList();
+
+        if (!changedOrAddedElements.Any()) return;
+
+        _joinCommand.Raise(changedOrAddedElements);
+    }
+}
---
+++ JoinCommand_EventHandler.cs
@@ -0,0 +1,66 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System.Collections.Generic;
+using System.Linq;
+
+[TransactionAttribute(TransactionMode.Manual)]
+[RegenerationAttribute(RegenerationOption.Manual)]
+
+public class JoinCommand_EventHandler : IExternalEventHandler
+{
+    #region Private Fields
+    private List<FamilyInstance> _addedElements = new List<FamilyInstance>();
+    private ExternalEvent _externalEvent;
+    #endregion
+
+    #region Constructor
+    public JoinCommand_EventHandler()
+    {
+        Initialize();
+    }
+
+    public void Initialize()
+    {
+        _externalEvent = ExternalEvent.Create(this);
+    }
+
+    public void Raise(List<FamilyInstance> addedElements)
+    {
+        _addedElements = addedElements;
+        _externalEvent.Raise();
+    }
+    #endregion
+    //------------------------- Methods -----------------------------------------------------
+
+    public void Execute(UIApplication app)
+    {
+        if (_addedElements == null || !_addedElements.Any()) return;
+
+        Document doc = app.ActiveUIDocument?.Document;
+        if (doc == null) return;
+
+        using (Transaction trans = new Transaction(doc, "Join Openings"))
+        {
+            trans.Start();
+
+            foreach (FamilyInstance familyInstance in _addedElements)
+            {
+                Element host = familyInstance.Host;
+
+                if (host != null && !JoinGeometryUtils.AreElementsJoined(doc, familyInstance, host))
+                {
+                    JoinGeometryUtils.JoinGeometry(doc, familyInstance, host);
+                }
+            }
+
+            trans.Commit();
+        }
+    }
+
+
+    public string GetName()
+    {
+        return "JoinCommand_EventHandler";
+    }
+}
